5f2550b09c935f0fa7282f03dd2888451021d6ad
michael <michael@laptopm>
2015-06-15 10:17:18 +0200

    startpage

--- src/Startpage.java
+++ src/Startpage.java
@@ -5,6 +5,12 @@
  */
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
@@ -22,7 +28,7 @@ public class Startpage extends JFrame {
     private final JLabel TRON = new JLabel();
     private final JButton Ok = new JButton();
     private final JTextField NamenEingabeFeld = new JTextField();
-    private final ImageIcon BackgroundIcon = new ImageIcon("BikeBackground.jpg");
+    private BufferedImage image=null;
 
     public Startpage(String title) {
         super(title);
@@ -36,9 +42,12 @@ public class Startpage extends JFrame {
         setLocation(x, y);
         setResizable(false);
         this.setLayout(null);
-        Background.setBounds(0, 0, 800, 600);
-        Background.setIcon(BackgroundIcon);
-        this.add(Background);
+        try {
+            image = ImageIO.read(new File("BikeBackground.jpg"));
+        } catch (IOException ex) {
+            Logger.getLogger(Startpage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
 
         NamenEingabeFeld.setBounds(300, 300, 200, 25);
         NamenEingabeFeld.setFont(new Font("Consolas", Font.PLAIN, 12));
@@ -49,6 +58,18 @@ public class Startpage extends JFrame {
                 NamenEingabeFeld_ActionPerformed(evt);
             }
         });
+          
+          Container c=new Container(){
+                public void paint( Graphics g ) { 
+                    g.drawImage(image, 0, 0, null);
+    super.paint(g);
+    
+  }
+          };
+         
+          
+        c.setBackground(new Color(0,0,0,0));
+        this.setContentPane(c);
         this.add(NamenEingabeFeld);
 
         NamenEingeben.setBounds(300, 240, 200, 20);
